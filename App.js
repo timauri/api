ecfeacbfe0b94896bac02d291322f6e37827b536
timauri <timaurileecarby@gmail.com>
2023-11-04 14:54:47 -0500

    pushing code

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text> Mr.Pres</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const allCaps = (text) => {
+  setAllCapsText(text.toUpperCase())
+}
