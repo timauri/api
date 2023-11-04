502b44a0c321ad774af4e9feaf22185a371f9632
timauri <timaurileecarby@gmail.com>
2023-11-04 15:01:51 -0500

    saving changes

--- App.js
+++ App.js
@@ -7,12 +7,57 @@ export default function App() {
     <View style={styles.container}>
       <Text> Mr.Pres</Text>
       <StatusBar style="auto" />
+      <div className="App">
+                <h1 className="geeks">GeeksforGeeks</h1>
+                <h3>Fetch data from an api in react</h3>
+                <div className="container">
+                    {items.map((item) => (
+                        <div className="item">
+                            <ol key={item.id}>
+                                <div>
+                                    <strong>
+                                        {"User_Name: "}
+                                    </strong>
+                                    {item.username},
+                                </div>
+                                <div>
+                                    Full_Name: {item.name},
+                                </div>
+                                <div>
+                                    User_Email: {item.email}
+                                </div>
+                            </ol>
+                        </div>
+                    ))}
+                </div>
+            </div>
+      
     </View>
   );
 }
 
+componentDidMount() 
+  fetch("https://www.tronalddump.io/")
+      .then((res) => res.json())
+      .then((json) => {
+          this.setState({
+              items: json,
+              DataisLoaded: true,
+          });
+      });
+
+
+render() 
+  const { DataisLoaded, items } = this.state;
+  if (!DataisLoaded)
+      return (
+          <div>
+              <h1> Pleses wait some time.... </h1>
+          </div>
+      );
+
 const styles = StyleSheet.create({
-  container: {
+  containger: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
